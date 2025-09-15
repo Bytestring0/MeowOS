dbedd43516575fce70a942f21a43ffa31c705922
Bytestring0 <Au2450126071@outlook.com>
2025-09-15 13:40:09 +0800

    1

---
+++ src/system-apps/framework/Framework.vue
@@ -0,0 +1,18 @@
+<template>
+
+<iframe :src="iframeSrc" frameborder="0"></iframe>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const iframeSrc = ref('https://peergame.cn/')
+</script>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
